73602ba8c504e5401568b0dda8652baf13414a50
Daniel Duckworth <18698360+danielduckworth@users.noreply.github.com>
2022-11-22 19:46:58 +1100

    Revert "Deploying to main from @ danielduckworth/blockly-terminal@e81636de495f49c81f6ee6abdd206737b5a5a739 🚀"
    This reverts commit 9b56541ed5ef9abe35dd48c479372628f001c59a.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import eggsWorkspaceVue from "./workspaces/eggs/index.vue";
+</script>
+
+<template>
+  <div id="app">
+    <eggsWorkspaceVue />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
